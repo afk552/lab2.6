ad62d90e0f96a746717487adae9bc6f0e1c19c95
afk552 <ez552@mail.ru>
2022-12-21 00:39:11 +0300

    Improve indiv task

--- 2.6_programs/indiv_1_12.py
+++ 2.6_programs/indiv_1_12.py
@@ -2,12 +2,30 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import datetime as dt
 
 if __name__ == '__main__':
     people = []
     print("Программа запущена, введите help для просмотра команд!")
+    # Словарь месяцев по их названию для select
+    month_by_text = {
+        "январь": "01",
+        "февраль": "02",
+        "март": "03",
+        "апрель": "04",
+        "май": "05",
+        "июнь": "06",
+        "июль": "07",
+        "август": "08",
+        "сентябрь": "09",
+        "октябрь": "10",
+        "ноябрь": "11",
+        "декабрь": "12",
+    }
+
     while True:
         command = input(">>> ").lower()
+        # Команда выхода из программы
         if command == "exit":
             break
 
@@ -15,12 +33,21 @@ if __name__ == '__main__':
         elif command == "add":
             name = input("Введите фамилию и имя через пробел: ")
             pnumber = input("Введите номер телефона: ")
-            birth = input("Введите дату рождения (01.01.2077): ")
+            try:
+                birth = input("Введите дату рождения (01.01.2077): ").split('.')
+                birth_dt = dt.datetime(int(birth[2]),
+                                       int(birth[1]),
+                                       int(birth[0])
+                                       )
+            except IndexError:
+                print("Проверьте правильность ввода даты рождения!",
+                      file=sys.stderr)
+                break
             # Создание словаря
             person = {
                 'name': name,
                 'pnumber': pnumber,
-                'birth': birth
+                'birth': birth_dt
             }
             # Добавление в список людей
             people.append(person)
@@ -54,26 +81,34 @@ if __name__ == '__main__':
                         nmbr,
                         person.get('name', ''),
                         person.get('pnumber', ''),
-                        person.get('birth', '')
+                        person.get('birth', '').strftime("%d.%m.%Y")
                     )
                 )
             print(line)
 
         # Выборка людей по введенному месяцу
         elif command.startswith('select '):
+            # Делим команду
             parts = command.split(' ', maxsplit=1)
+            # Переменная коррекции месяца при вводе одной цифры
             printed_month_correct = ""
             printed_month = parts[1]
+            # Если ввели месяц как текст, ищем значение в словаре
+            if printed_month.isalpha():
+                printed_month.lower()
+                for key, value in month_by_text.items():
+                    if key == printed_month:
+                        printed_month = value
             if len(printed_month) == 1:
                 printed_month_correct += "0"
                 printed_month_correct += printed_month
             else:
                 printed_month_correct = printed_month
+
             count = 0
             for person in people:
-                birth = person.get('birth', 0)
-                b_spl = birth.split('.')
-                if printed_month_correct == b_spl[1]:
+                birth = person.get('birth')
+                if printed_month_correct == birth.strftime("%m"):
                     count += 1
                     print(
                         '{:>4}: {}'.format(count, person.get('name', ''))
@@ -86,7 +121,7 @@ if __name__ == '__main__':
             print("Список доступных команд:")
             print("add - добавить человека;")
             print("list - вывести список людей;")
-            print("select <месяц> - запросить людей, чьи "
+            print("select <месяц> ('Январь' / '01') - запросить людей, чьи "
                   "дни рождения приходятся на указанный месяц;")
             print("help - отобразить справку;")
             print("exit - завершить работу с программой.")
